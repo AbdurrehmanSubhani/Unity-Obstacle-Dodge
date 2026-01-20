194ecb6d7a88e2c3e2bb9cfb385fd837b83e5132
AbdurrehmanSubhani <aarrsubhani@gmail.com>
2026-01-20 17:08:11 +0500

    feat: re-org hazards

--- Assets/Scripts/TriggerProjectile.cs
+++ Assets/Scripts/TriggerProjectile.cs
@@ -4,6 +4,10 @@ public class TriggerProjectile : MonoBehaviour
 {
     [SerializeField] GameObject projectile;
 
+    private void Awake() {
+        projectile.SetActive(false);
+    }
+    
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
             projectile.SetActive(true);
